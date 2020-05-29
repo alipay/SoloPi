6917ecd6217b3d2368e5e48ba1c229edf023ff48
ruikai.qrk <ruikai.qrk@antfin.com>
2020-05-29 10:58:30 +0800

    控件查找fix

--- src/shared/src/main/java/com/alipay/hulu/shared/node/utils/OperationUtil.java
+++ src/shared/src/main/java/com/alipay/hulu/shared/node/utils/OperationUtil.java
@@ -15,11 +15,13 @@
  */
 package com.alipay.hulu.shared.node.utils;
 
-import android.graphics.Point;
+import android.content.Context;
 import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
+import com.alipay.hulu.common.application.LauncherApplication;
 import com.alipay.hulu.common.tools.CmdTools;
-import com.alipay.hulu.common.utils.DeviceInfoUtil;
 import com.alipay.hulu.common.utils.LogUtil;
 import com.alipay.hulu.common.utils.MiscUtil;
 import com.alipay.hulu.common.utils.StringUtil;
@@ -134,8 +136,13 @@ public class OperationUtil {
             AbstractNodeTree operationNode;
             int pos = 0;
 
+            DisplayMetrics dm = new DisplayMetrics();
+            ((WindowManager) LauncherApplication.getInstance().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRealMetrics(dm);
+            int height = dm.heightPixels;
+            int width = dm.widthPixels;
             int scrollCount = 0;
             do {
+                service.invalidRoot();
                 AbstractNodeTree tmpRoot = service.getCurrentRoot();
                 operationNode = OperationNodeLocator.findAbstractNode(tmpRoot, node);
 
@@ -149,28 +156,23 @@ public class OperationUtil {
                 if (!(operationNode instanceof CaptureTree)) {
                     // 判断下是否在屏幕内
                     Rect bound = operationNode.getNodeBound();
-                    Point screenSize = DeviceInfoUtil.realScreenSize;
-                    LogUtil.d(TAG, "控件空间属性：%s, 屏幕属性：%s",  bound, screenSize);
-                    if (bound.top <= 5 && bound.bottom <= 5) {
+                    LogUtil.d(TAG, "控件空间属性：%s, 屏幕属性：%s",  bound, dm);
+                    if (bound.bottom <= 5) {
                         service.doSomeAction(new OperationMethod(PerformActionEnum.GLOBAL_SCROLL_TO_BOTTOM), null);
                         MiscUtil.sleep(2500);
                         scrollCount ++;
-                        service.invalidRoot();
-                    } else if (bound.top >= screenSize.y - 5) {
+                    } else if (bound.top >= height - 5) {
                         service.doSomeAction(new OperationMethod(PerformActionEnum.GLOBAL_SCROLL_TO_TOP), null);
                         MiscUtil.sleep(2500);
                         scrollCount ++;
-                        service.invalidRoot();
                     } else if (bound.centerX() <= 5) {
                         service.doSomeAction(new OperationMethod(PerformActionEnum.GLOBAL_SCROLL_TO_RIGHT), null);
                         MiscUtil.sleep(2500);
                         scrollCount ++;
-                        service.invalidRoot();
-                    } else if (bound.centerX() >= screenSize.x - 5) {
+                    } else if (bound.centerX() >= width - 5) {
                         service.doSomeAction(new OperationMethod(PerformActionEnum.GLOBAL_SCROLL_TO_LEFT), null);
                         MiscUtil.sleep(2500);
                         scrollCount ++;
-                        service.invalidRoot();
                     } else {
                         pos = 1;
                     }
@@ -223,6 +225,20 @@ public class OperationUtil {
      * @return
      */
     public static AbstractNodeTree findAbstractNode(OperationNode node, OperationService service, List<String> prepareActions) {
+
+        AbstractNodeTree tmpRoot = null;
+        try {
+            service.invalidRoot();
+            tmpRoot = service.getCurrentRoot();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 拿不到root
+        if (tmpRoot == null) {
+            return null;
+        }
+
         AbstractNodeTree targetNode = null;
 
         // 两次处理弹窗
